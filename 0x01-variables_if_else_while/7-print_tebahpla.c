5003da0762a6dbb69f3062acc0acf15353c14b7b
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-14 13:03:50 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse.
+ * followes by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '2'; ch >= 'a'; ch--)
+	[
+		putchar9ch);
+
+	}
+	putchar('\n');
+	return (0);
+}
