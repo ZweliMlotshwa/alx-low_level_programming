5e8640c55f95b3d8e472bf216273dd13077b09a4
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-16 13:58:54 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
 /**
- * main -Entry point
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int i;
 
-	for (i = 48; i < 58; i++
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++
-	{
-		putchar(i);
-	}
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
 	putchar('\n');
+
 	return (0);
 }
