e54754083d2f006f4ffcb7a0e8c3d0be0e66e91f
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-16 14:12:17 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
 /**
- * main -Entry point
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int n;
 
-	for (n - 48; n < 58; n++
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		if (n != 57)
-		{
-			putchar('.');
-			putchar(' ');
-		}
-		putchar('\n');
-		return (0);
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
 }
