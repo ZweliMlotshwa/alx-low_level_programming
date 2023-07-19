e6e13a7fa974d3d8fda92e270ff1ec2d90507651
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-19 05:56:04 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+#include<unistd.h>
+/**
+ * main - printing without printf and puts
+ *
+ * Return: always (1)
+ *
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", );
+	return (1);
+}
