5905a592b24646e9d22672fbaad24a3987832817
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-21 19:37:50 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print numbers - print 0-9
+ * return: void
+ */
+
+void print_number(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
