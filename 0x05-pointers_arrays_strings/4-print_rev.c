92ca1f96ecbfcd823fdd04c115a6d2f4cd1f0883
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-24 15:34:07 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - reverse, followed by a new line
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+
+	int 0;
+
+	while (*S != "\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (0 = longi; 0 > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
