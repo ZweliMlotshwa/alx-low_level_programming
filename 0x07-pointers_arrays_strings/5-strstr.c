cb9646734c2e0bd19a1ab968392aac0ce0597d1b
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-07-31 13:43:57 -0700

    0x07 pointers arrays strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,8 +9,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *1 = haystack;
+		char * 1 = haystack;
 		char *p = needle;
+
 		while (*1 == *p && *p != '\0')
 		{
 			l++;
