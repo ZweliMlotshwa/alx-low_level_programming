818e6ca44f8a971d0e706b7bede1340218897959
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-09 09:41:27 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1, len2, i, j;
-	char *ptr;
+	char *conct;
+	int i, ci
 
 	if (s1 == NULL)
 	{
@@ -24,35 +24,28 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
 
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (ptr == NULL)
-	{
-		free(ptr);
+	if (conct == NULL)
 		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		ptr[i] = s[i];
-	}
-
-	for (j = 0; j <= len2; j++)
-	{
-		ptr[i] = s2[j];
+		conct[i] = s1[i];
 		i++;
 	}
-	return (ptr);
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/**
- * free_grid - frees 2nd array
- * @grid: 2nd grid
- * @height: height dimension of grid
- * Description: frees memory of grid
- * Return: nothing
- *
- */
-void free_grid(int **grid, int height)
-{
-	int;
 
-	for (i = 0; i < height; i++)
+/**
+* alloc_grid - creates a 2D grid of integers
+* @width: width of the grid
+* @height: height of the grid
+*
+* Return: pointer to the created grid, or NULL on failure
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+	return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
 	{
-		free(grid[i]);
+	grid[x] = malloc(sizeof(int) * width);
+
+	if (grid[x] == NULL)
+		{
+		for (y = x - 1; y >= 0; y--)
+		free(grid[y]);
+
+		free(grid);
+		return (NULL);
+		}
 	}
-	free(grid);
+
+	for (x = 0; x < height; x++)
+	{
+	for (y = 0; y < width; y++)
+	grid[x][y] = 0;
+	}
+
+	return (grid);
 }
+
