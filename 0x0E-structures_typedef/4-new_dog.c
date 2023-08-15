e0434dd777fbb85e74c1d808bdbfbd866e3381b3
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-15 02:16:06 -0700

    0x0E.C-Structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -70,9 +70,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->name == NULL)
 	{
 		free(dog);
-		free(dog->name);
 		return (NULL);
 	}
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+}
 
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
