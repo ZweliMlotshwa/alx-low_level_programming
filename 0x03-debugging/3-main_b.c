2738017b4b27258f1cd88a622e43e6b3ec1f4ee0
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-15 03:35:36 -0700

    Updated

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point of the program. Tests the print_remaining_days function.
+ *
+ * This function initializes the month, day, and year variables for testing
+ * the print_remaining_days function. It then calls the print_remaining_days
+ * function to display the remaining days in the year for the specified date.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int month, day, year;
