eb36ce20ed208603d688097181326d0d8f86c212
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-16 06:40:12 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -9,7 +9,7 @@
  */
 int is_98(int elem)
 {
-	return (98 == elem);
+	return (-elem == 98);
 }
 
 /**
@@ -32,7 +32,7 @@ int is_strictly_positive(int elem)
  */
 int abs_is_98(int elem)
 {
-	return (elem == 98 || -elem == 98);
+	return (elem == 98 || -elem == -98);
 }
 
 /**
@@ -42,14 +42,18 @@ int abs_is_98(int elem)
  */
 int main(void)
 {
-	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98,
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
 	int index;
 
 	index = int_index(array, 20, is_98);
-	printf("%d\n", index);
+	printf("Index for is_98: %d\n", index);
+
 	index = int_index(array, 20, abs_is_98);
-	printf("%d\n", index);
+	printf("Index for abs_is_98: %d\n", index);
+
 	index = int_index(array, 20, is_strictly_positive);
-	printf("%d\n", index);
+	printf("Index for is_strictly_positive: %d\n", index);
+
 	return (0);
 }
