3ec21a7e8af282533d6d297123ebe2c398b71607
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-26 00:09:33 -0700

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
 section .data
-    hello db "Hello, Holberton",0
-    format db "%s",0
-    newline db 10,0
+	hello db "Hello, Holberton",0
+	format db "%s",0
+	newline db 10,0
 
 section .text
-    global main
+	global main
 
 extern printf
 
 main:
-    push rbp
-    mov rdi, format
-    mov rsi, hello
-    call printf
-    mov rdi, format
-    mov rsi, newline
-    call printf
-    pop rbp
-    ret
+	push rbp
+	mov rdi, format
+	mov rsi, hello
+	call printf
+	mov rdi, format
+	mov rsi, newline
+	call printf
+	pop rbp
+	ret
 
