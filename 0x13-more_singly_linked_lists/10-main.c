d3f243896d95b902dd07538757a400c348319bf7
ZweliMlotshwa <zwelimlotshwah@gmail.com>
2023-08-29 14:15:29 -0700

    Updated

--- 0x13-more_singly_linked_lists/10-main.c
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -10,22 +10,27 @@
  */
 int main(void)
 {
-	listint_t *head = NULL;
+	listint_t *head;
 
-	for (int i = 1; i <= 10; i++)
-	{
-	add_nodeint_end(&head, i * 100);
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
 	print_listint(head);
 	printf("-----------------\n");
-	}
 
-	for (int i = 0; i < 10; i++)
+/*Delete nodes while the list is not empty*/
+	while (head != NULL)
 	{
 	delete_nodeint_at_index(&head, 0);
+	print_listint(head);
 	printf("-----------------\n");
 	}
-
-	print_listint(head);
 
 	return (0);
 }
